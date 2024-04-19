416dc30ec50ddbcc1f8dda79f456fcb67056629b
Lucas Rezende <lucasffrezende@gmail.com>
2024-04-19 16:30:08 -0300

    ✨ Adicionado página de Locutor

---
+++ src/pages/Locutor.jsx
@@ -0,0 +1,10 @@
+import MenuApp from "../components/MenuApp";
+
+export default function PageLocutor() {
+    return (
+        <div>
+            <MenuApp />
+            Página de Locutores
+        </div>
+    )
+}
