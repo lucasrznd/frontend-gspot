7f997091a76da1196dead5225f6fcb99ba13d7ed
Lucas Rezende <lucasffrezende@gmail.com>
2024-04-19 15:43:29 -0300

    ✨ Adicionado model de Spot

---
+++ src/models/SpotModel.js
@@ -0,0 +1,14 @@
+export class SpotModel {
+
+    constructor(codigo, titulo, empresa, locutor, data, duracao, contratoAtivo, preco) {
+        this.codigo = codigo;
+        this.titulo = titulo;
+        this.empresa = empresa;
+        this.locutor = locutor;
+        this.data = data;
+        this.duracao = duracao;
+        this.contratoAtivo = contratoAtivo;
+        this.preco = preco;
+    }
+
+}
