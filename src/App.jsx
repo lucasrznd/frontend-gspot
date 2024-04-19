ec973e1063548e759b33a0af35f3528d5ca9da92
Lucas Rezende <lucasffrezende@gmail.com>
2024-04-19 16:13:01 -0300

    ♻️ Importado menu

--- src/App.jsx
+++ src/App.jsx
@@ -2,24 +2,13 @@ import './App.css';
 import 'primereact/resources/themes/lara-light-blue/theme.css';
 import 'primeicons/primeicons.css';
 import 'primeflex/primeflex.css';
+import MenuApp from './components/MenuApp';
 
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MenuApp />
     </div>
   );
 }
