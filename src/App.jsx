a3a1cfd49bb1b17bbd30f653a455079164299db1
Lucas Rezende <lucasffrezende@gmail.com>
2024-04-19 15:36:05 -0300

    🔥 Removido arquivos não usados, e feito imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import 'primereact/resources/themes/lara-light-blue/theme.css';
+import 'primeicons/primeicons.css';
+import 'primeflex/primeflex.css';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
