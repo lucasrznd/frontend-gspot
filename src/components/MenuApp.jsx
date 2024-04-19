8eb323c212bcadb108fd1d62cf2c7592317803e3
Lucas Rezende <lucasffrezende@gmail.com>
2024-04-19 16:12:20 -0300

    ✨ Adicionado Menu da aplicação

---
+++ src/components/MenuApp.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import logo from '../../src/assets/images/logo-horizontal.png';
+import { MegaMenu } from 'primereact/megamenu';
+
+export default function MenuApp() {
+
+    const items = [
+        {
+            label: 'Spots',
+            icon: 'pi pi-microphone',
+            url: '/contrato'
+        },
+        {
+            label: 'Empresas',
+            icon: 'pi pi-building',
+            url: '/parcela'
+        },
+        {
+            label: 'Locutores',
+            icon: 'pi pi-users',
+            url: '/imovel'
+        }
+    ];
+
+    const start = <a href='/'><img alt="logo" src={logo} height={60} className="mr-2" /></a>
+
+    return (
+        <div className="card">
+            <MegaMenu model={items} orientation="horizontal" start={start} breakpoint="600px" className="p-3 surface-0 shadow-2 mb-2" style={{ borderRadius: '1rem' }} />
+        </div>
+    )
+}
