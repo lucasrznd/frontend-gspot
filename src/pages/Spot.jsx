1e60e8bb14f5a1799b222794f53bd59e02b32aca
Lucas Rezende <lucasffrezende@gmail.com>
2024-04-19 16:30:45 -0300

    ✨ Adicionado página de Spots

---
+++ src/pages/Spot.jsx
@@ -0,0 +1,10 @@
+import MenuApp from "../components/MenuApp";
+
+export default function PageSpot() {
+    return (
+        <div>
+            <MenuApp/>
+            <h1>Pagina de Spots</h1>
+        </div>
+    )
+}
