7b3bbd6248f5f9cff0c6f2ea6da1b7b4b72ac303
Lucas Rezende <lucasffrezende@gmail.com>
2024-04-19 16:30:27 -0300

    ✨ Adicionado página de Empresa

---
+++ src/pages/Empresa.jsx
@@ -0,0 +1,10 @@
+import MenuApp from "../components/MenuApp";
+
+export default function PageEmpresa() {
+    return (
+        <div>
+            <MenuApp />
+            Página de Empresas
+        </div>
+    )
+}
