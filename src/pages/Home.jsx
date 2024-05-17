d8fb00809d0e8d948ba545b5ce6d4c273a98e33d
Lucas Rezende <lucasffrezende@gmail.com>
2024-05-17 17:33:39 -0300

    ✨ Adicionado página Home

---
+++ src/pages/Home.jsx
@@ -0,0 +1,8 @@
+import MenuApp from "../components/MenuApp";
+
+export default function HomePage() {
+    return <div>
+        <MenuApp />
+        <h2>This is Home Page.</h2>
+    </div>
+}
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,15 +1,17 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
-import PageSpot from "../pages/Spot"
-import PageEmpresa from "../pages/Empresa"
-import PageLocutor from "../pages/Locutor"
+import PageSpot from "../pages/Spot";
+import PageEmpresa from "../pages/Empresa";
+import LocutorPage from "../pages/Locutor";
+import HomePage from "../pages/Home";
 
 const AppRoutes = () => {
     return (
         <Router>
             <Routes>
-                <Route path="/" element={<PageSpot />} />
+                <Route path="/" element={<HomePage />} />
+                <Route path="/spots" element={<PageSpot />} />
                 <Route path="/empresa" element={<PageEmpresa />} />
-                <Route path="/locutor" element={<PageLocutor />} />
+                <Route path="/locutor" element={<LocutorPage />} />
             </Routes>
         </Router>
     )
