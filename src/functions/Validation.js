92a0cd4c71d1ef544e65bb389590206335a2aad8
Lucas Rezende <lucasffrezende@gmail.com>
2024-05-22 13:17:46 -0300

    ✨ Adicionado function para validar link de imagem

---
+++ src/functions/Validation.js
@@ -0,0 +1,8 @@
+export function isImgUrl(url) {
+    const img = new Image();
+    img.src = url;
+    return new Promise((resolve) => {
+        img.onerror = () => resolve(false);
+        img.onload = () => resolve(true);
+    });
+}
