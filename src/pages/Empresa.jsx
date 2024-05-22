a6216ed9a56f2817917f647d8f5554b16c6a86e9
Lucas Rezende <lucasffrezende@gmail.com>
2024-05-22 13:23:23 -0300

    🎨 Adicionado title na página de Empresa

--- src/pages/Empresa.jsx
+++ src/pages/Empresa.jsx
@@ -1,10 +1,20 @@
 import MenuApp from "../components/MenuApp";
+import Rodape from "../components/Rodape";
+import FormEmpresa from "../components/empresa/FormEmpresa";
+import { Helmet, HelmetProvider } from "react-helmet-async";
 
-export default function PageEmpresa() {
+export default function EmpresaPage() {
     return (
         <div>
+            <HelmetProvider>
+                <Helmet>
+                    <title>{"EducadoraGS - Empresas"}</title>
+                </Helmet>
+            </HelmetProvider>
+
             <MenuApp />
-            Página de Empresas
+            <FormEmpresa />
+            <Rodape />
         </div>
     )
 }
