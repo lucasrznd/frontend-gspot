485337d7e684cac7eb6b12ea2bd3c137be495a99
Lucas Rezende <lucasffrezende@gmail.com>
2024-05-22 13:20:03 -0300

    ✨ Alterado componentes de locutor

--- src/components/locutor/FormLocutor.jsx
+++ src/components/locutor/FormLocutor.jsx
@@ -1,69 +1,177 @@
-import React, { useRef, useState } from "react";
-import { LocutorModel } from "../../models/LocutorModel";
+import React, { useEffect, useRef, useState } from "react";
 import TableLocutor from "./TableLocutor";
 import { Dialog } from "primereact/dialog";
 import { InputText } from "primereact/inputtext";
 import { InputMask } from "primereact/inputmask";
 import { Button } from "primereact/button";
 import { Toast } from "primereact/toast";
-import { msgInformativa } from "../../functions/Mensagens";
+import { msgErro, msgSucesso } from "../../functions/Mensagens";
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
+import { useLocutorMutate } from "../../hooks/useLocutorMutate";
+import { useLocutorPut } from "../../hooks/useLocutorPut";
+import { Avatar } from "primereact/avatar";
+import ImageDialog from "../ImageDialog";
+import { isImgUrl } from "../../functions/Validation";
 
 export default function FormLocutor(props) {
     const [visualizarModal, setVisualizarModal] = useState(false);
+    const [imageVisible, setImageVisible] = useState(false);
     const toast = useRef(null);
+    const { mutate, isSuccess } = useLocutorMutate();
+    const { mutate: mutatePut } = useLocutorPut();
 
-    function novoLocutor() {
-        props.setLocutor(new LocutorModel());
+    const validationSchema = Yup.object().shape({
+        nome: Yup.string().required('Nome é obrigatório'),
+    });
+
+    const formik = useFormik({
+        initialValues: {
+            id: undefined,
+            nome: '', // Initial value of "nome" field
+            telefone: '', // Initial value of "telefone" field
+            urlImage: ''
+        },
+        validationSchema: validationSchema, // Validation Schema
+        onSubmit: async (values, actions) => {
+            const data = values;
+
+            if (data.urlImage !== '') {
+                if (await isImgUrl(data.urlImage) === false) {
+                    msgErro(toast, 'Link de imagem inválido.');
+                    return;
+                }
+            }
+
+            // If id != null is made a PUT Request, else a POST Request
+            data.id !== undefined ? mutatePut(data) : mutate(data);
+            closeModalForm();
+            actions.resetForm();
+            msgSucesso(toast, 'Locutor salvo com sucesso.');
+        },
+    });
+
+    useEffect(() => {
+        closeModalForm();
+    }, [isSuccess]);
+
+    const isFormFieldValid = (name) => !!(formik.touched[name] && formik.errors[name]);
+
+    const getFormErrorMessage = (name) => {
+        return isFormFieldValid(name) && <small className="p-error">{formik.errors[name]}</small>;
+    };
+
+    function newLocutor() {
+        formik.resetForm();
+        formik.values.id = undefined;
+        formik.values.nome = '';
+        formik.values.telefone = '';
+        formik.values.urlImage = '';
         setVisualizarModal(true);
     }
 
-    const salvarLocutorAction = () => {
+    const closeModalForm = () => {
         setVisualizarModal(false);
-        msgInformativa(toast, 'Locutor salvo com sucesso.');
     }
 
-    const detalhesLocutor = (locutor) => {
-        props.setLocutor({ ...locutor });
+    const locutorDetails = (locutor) => {
+        formik.resetForm();
+        formik.values.id = locutor.id;
+        formik.values.nome = locutor.nome;
+        formik.values.telefone = locutor.telefone;
+        formik.values.urlImage = locutor.urlImage;
         setVisualizarModal(true);
     };
 
-    const fecharModal = () => {
+    const closeModal = () => {
         setVisualizarModal(false);
     }
 
-    const conteudoInicial = (
+    const startContent = (
         <React.Fragment>
-            <Button icon="pi pi-plus-circle" label='Novo' onClick={novoLocutor} />
+            <Button icon="pi pi-plus-circle" label='Novo' onClick={newLocutor} />
         </React.Fragment>
     );
 
-    const rodapeModal = (
+    const modalFooter = (
         <div>
-            <Button label="Salvar" icon="pi pi-check" onClick={salvarLocutorAction} autoFocus />
-            <Button label="Cancelar" icon="pi pi-times" outlined onClick={fecharModal} />
+            <Button label="Salvar" type="submit" icon="pi pi-check" onClick={formik.handleSubmit} autoFocus />
+            <Button label="Cancelar" icon="pi pi-times" outlined onClick={closeModal} />
         </div>
     );
+
+    const openImageDialog = () => {
+        formik.values.urlImage !== '' ? setImageVisible(true) : setImageVisible(false);
+    }
+
+    const closeImageDialog = () => {
+        setImageVisible(false);
+    }
 
     return (
         <div>
             <Toast ref={toast} />
 
-            <TableLocutor conteudoInicial={conteudoInicial} detalhesLocutor={detalhesLocutor}
-                locutor={props.locutor} setLocutor={props.setLocutor} locutores={props.locutores} />
+            <TableLocutor startContent={startContent} locutorDetails={locutorDetails} setLocutor={props.setLocutor} />
 
-            <Dialog header="Detalhes do Locutor" visible={visualizarModal} style={{ width: '30vw', minWidth: "30vw" }} onHide={() => setVisualizarModal(false)}
-                footer={rodapeModal} draggable={false}>
+            <Dialog header="Detalhes do Locutor" visible={visualizarModal} style={{ width: '40vw', minWidth: "40vw" }} breakpoints={{ '960px': '65vw', '641px': '70vw' }} onHide={() => setVisualizarModal(false)}
+                footer={modalFooter} draggable={false}>
                 <div className="card p-fluid">
-                    <div className="field">
-                        <label htmlFor='nome' style={{ marginBottom: '0.5rem' }}>Nome:</label>
-                        <InputText id="nome" value={props.locutor?.nome} onChange={(e) => props.setLocutor({ ...props.locutor, nome: e.target.value })} className="uppercase" />
-                    </div>
+                    <form onSubmit={formik.handleSubmit}>
+                        <div className="field flex align-items-center justify-content-center">
+                            {/* <img src={formik.values.urlImage} alt="Adicione a imagem." onClick={openImageDialog} className='shadow-4 cursor-pointer border-circle' width='80px' height='80px' /> */}
+                            <Avatar icon='pi pi-user' image={formik.values.urlImage} onClick={openImageDialog} size="xlarge" className="mr-2 shadow-4" shape="circle" />
+                            <ImageDialog visible={imageVisible} onHide={closeImageDialog} header="Imagem do Locutor" src={formik.values.urlImage} />
+                        </div>
 
-                    <div className="field">
-                        <label htmlFor='telefone' style={{ marginBottom: '0.5rem' }}>Telefone:</label>
-                        <InputMask id="telefone" value={props.locutor?.telefone} onChange={(e) => props.setLocutor({ ...props.locutor, telefone: e.target.value })}
-                            mask="(99) 9 9999-9999" placeholder="(99) 9 9999-9999" />
-                    </div>
+                        <div className="field">
+                            <label htmlFor='nome' style={{ marginBottom: '0.5rem' }}>Nome:</label>
+                            <div className="p-inputgroup flex-1">
+                                <span className="p-inputgroup-addon">
+                                    <i className="pi pi-user"></i>
+                                </span>
+                                <InputText
+                                    id="nome"
+                                    name="nome"
+                                    value={formik.values.nome}
+                                    onChange={formik.handleChange}
+                                    className={isFormFieldValid('nome') ? "p-invalid uppercase" : "uppercase"}
+                                />
+                            </div>
+                            {getFormErrorMessage('nome')}
+                        </div>
+
+                        <div className="field">
+                            <label htmlFor='telefone' style={{ marginBottom: '0.5rem' }}>Telefone:</label>
+                            <div className="p-inputgroup flex-1">
+                                <span className="p-inputgroup-addon">
+                                    <i className="pi pi-phone"></i>
+                                </span>
+                                <InputMask
+                                    id="telefone"
+                                    name="telefone"
+                                    value={formik.values.telefone}
+                                    onChange={formik.handleChange}
+                                    mask="(99) 9 9999-9999"
+                                    placeholder="(99) 9 9999-9999"
+                                />
+                            </div>
+                        </div>
+
+                        <div className="field">
+                            <label htmlFor='urlImage' style={{ marginBottom: '0.5rem' }}>Imagem:</label>
+                            <div className="p-inputgroup flex-1">
+                                <span className="p-inputgroup-addon">
+                                    <i className="pi pi-image"></i>
+                                </span>
+                                <InputText
+                                    id="urlImage"
+                                    name="urlImage"
+                                    value={formik.values.urlImage}
+                                    onChange={formik.handleChange} />
+                            </div>
+                        </div>
+                    </form>
                 </div>
             </Dialog>
         </div>
--- src/components/locutor/TableLocutor.jsx
+++ src/components/locutor/TableLocutor.jsx
@@ -2,29 +2,51 @@ import { Panel } from 'primereact/panel';
 import { Toolbar } from 'primereact/toolbar';
 import { DataTable } from 'primereact/datatable';
 import { Column } from 'primereact/column';
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { Button } from 'primereact/button';
 import { InputText } from 'primereact/inputtext';
 import { IconField } from "primereact/iconfield";
 import { InputIcon } from "primereact/inputicon";
-import ModalDelete from '../ModalDelete';
+import DeleteDialog from '../DeleteDialog';
 import { formatarParaUppercase, formatarTelefone } from '../../functions/Formatacao';
+import { useLocutorDelete } from '../../hooks/useLocutorDelete';
+import { msgAviso, msgErro } from '../../functions/Mensagens';
+import { Toast } from 'primereact/toast';
+import { Tag } from 'primereact/tag';
+import { Avatar } from 'primereact/avatar';
+import { ProgressSpinner } from 'primereact/progressspinner';
+import { useLocutorData } from '../../hooks/useLocutorData';
+import ImageDialog from '../ImageDialog';
 
 export default function TableLocutor(props) {
+    const [locutor, setLocutor] = useState({});
+    const [imageVisible, setImageVisible] = useState(false);
     const [deleteLocutorDialog, setDeleteLocutorDialog] = useState(false);
     const [globalFilter, setGlobalFilter] = useState(null);
+    const toast = useRef(null);
+    const { data, isLoading, isError: isFetchError } = useLocutorData();
+    const { mutate, isSuccess, isError } = useLocutorDelete();
 
-    const deletarLocutor = async () => {
-        setDeleteLocutorDialog(false);
-        console.log('Locutor deletado');
+    const deleteLocutor = () => {
+        mutate(locutor.id);
+
+        if (isError) {
+            msgErro(toast, 'Erro ao remover locutor.');
+        } else {
+            msgAviso(toast, 'Locutor removido com sucesso.');
+        }
     }
 
+    useEffect(() => {
+        closeModal();
+    }, [isSuccess]);
+
     const confirmDeleteLocutor = (locutor) => {
-        props.setLocutor({ ...locutor });
+        setLocutor({ ...locutor });
         setDeleteLocutorDialog(true);
     };
 
-    const hideDeleteLocutorDialog = () => {
+    const closeModal = () => {
         setDeleteLocutorDialog(false);
     };
 
@@ -38,44 +60,71 @@ export default function TableLocutor(props) {
         </div>
     );
 
-    const acoesTable = (rowData) => {
+    const tableActions = (rowData) => {
         return (
             <React.Fragment>
-                <Button icon="pi pi-pencil" rounded onClick={() => props.detalhesLocutor(rowData)} />
+                <Button icon="pi pi-pencil" rounded onClick={() => props.locutorDetails(rowData)} />
                 <span style={{ marginBottom: "1rem", marginLeft: "5px", marginRight: "5px" }} className="pi pi-ellipsis-v"></span>
                 <Button icon="pi pi-trash" rounded severity="danger" onClick={() => confirmDeleteLocutor(rowData)} />
             </React.Fragment>
         );
     };
 
-    const deleteLocutorFooter = (
-        <React.Fragment>
-            <Button label="Cancelar" icon="pi pi-times" outlined onClick={hideDeleteLocutorDialog} />
-            <Button label="Sim" icon="pi pi-check" severity="danger" onClick={deletarLocutor} />
-        </React.Fragment>
-    );
+    const imageBody = (rowData) => {
+        // if (rowData.image === '') {
+        //     rowData.image = 'https://icons.veryicon.com/png/o/internet--web/prejudice/user-128.png';
+        // }
+
+        return <>
+            {/* <img src={rowData.image} alt="Imagem do Locutor" onClick={(e) => openTableImageDialog(e.target.currentSrc)} className='shadow-4 cursor-pointer border-circle' width='80px' height='80px' /> */}
+            <Avatar icon="pi pi-user" image={rowData.urlImage} onClick={(e) => openTableImageDialog(e.target.currentSrc)} size="xlarge" className="mr-2 shadow-4" shape="circle" />
+        </>
+    };
+
+    const imageTableHeader = () => {
+        return <div className='flex align-items-center'>
+            <p className='mr-1'>Imagem</p>
+            <Tag severity='secondary' value="Novo"></Tag>
+        </div>
+    }
+
+    const openTableImageDialog = (rowData) => {
+        setLocutor({ ...locutor, urlImage: rowData });
+        rowData !== undefined ? setImageVisible(true) : setImageVisible(false)
+    }
+
+    const closeTableImageDialog = () => {
+        setImageVisible(false);
+    }
 
     return (
         <div>
+            <Toast ref={toast} />
             <Panel>
-                <Toolbar style={{ marginBottom: "10px" }} start={props.conteudoInicial} />
+                <Toolbar style={{ marginBottom: "10px" }} start={props.startContent} />
+
+                {isLoading && <ProgressSpinner />}
+                {isFetchError && msgErro(toast, 'Erro ao carregar locutores.')}
 
                 <div className="card">
-                    <DataTable value={props.locutores} tableStyle={{ minWidth: '50rem' }}
+                    <DataTable value={data} tableStyle={{ minWidth: '50rem' }}
                         paginator globalFilter={globalFilter} header={header}
                         paginatorTemplate="FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink CurrentPageReport RowsPerPageDropdown"
                         currentPageReportTemplate="{first} de {last} de {totalRecords} locutores"
-                        rows={5} emptyMessage="Nenhum locutor encontrado." key="codigo">
-                        <Column field="codigo" header="Código" align="center" alignHeader="center"></Column>
+                        rows={5} emptyMessage="Nenhum locutor encontrado." key="id">
+                        <Column field="id" header="Código" align="center" alignHeader="center"></Column>
                         <Column field="nome" body={(rowData) => formatarParaUppercase(rowData, "nome")} header="Nome" align="center" alignHeader="center"></Column>
                         <Column field="telefone" body={(rowData) => formatarTelefone(rowData)} header="Telefone" align="center" alignHeader="center"></Column>
-                        <Column body={acoesTable} exportable={false} style={{ minWidth: '12rem' }} align="center" header="Ações" alignHeader="center"></Column>
+                        <Column field="urlImage" body={imageBody} header={imageTableHeader} align="center" alignHeader="center"></Column>
+                        <Column body={tableActions} exportable={false} style={{ minWidth: '12rem' }} align="center" header="Ações" alignHeader="center"></Column>
                     </DataTable>
                 </div>
             </Panel>
 
-            <ModalDelete deleteObjetoDialog={deleteLocutorDialog} deleteObjetoFooter={deleteLocutorFooter}
-                hideDeleteObjetoDialog={hideDeleteLocutorDialog} objeto={props.locutor} />
+            <ImageDialog visible={imageVisible} onHide={closeTableImageDialog} header="Imagem do Locutor" src={locutor.urlImage} />
+
+            <DeleteDialog deleteObjectDialog={deleteLocutorDialog} hideDeleteDialog={closeModal} deleteObject={deleteLocutor}
+                hideDeleteObjectDialog={closeModal} object={locutor} />
         </div>
     )
 
