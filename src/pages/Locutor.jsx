98778fa05daaa228e3cb4dfe892770ec1038c8b9
Lucas Rezende <lucasffrezende@gmail.com>
2024-05-22 13:20:45 -0300

    🎨 Adicionado title na pagina de Locutor

--- src/pages/Locutor.jsx
+++ src/pages/Locutor.jsx
@@ -1,24 +1,19 @@
-import { useState } from "react";
 import MenuApp from "../components/MenuApp";
 import FormLocutor from "../components/locutor/FormLocutor";
-import { LocutorModel } from "../models/LocutorModel";
 import Rodape from "../components/Rodape";
+import { Helmet, HelmetProvider } from "react-helmet-async";
 
-export default function PageLocutor() {
-    const [locutor, setLocutor] = useState(new LocutorModel());
-    const [locutores, setLocutores] = useState([
-        { codigo: 1, nome: "Reginaldo Serra", telefone: "43999999999" },
-        { codigo: 2, nome: "Paulo Netto", telefone: "43999999999" },
-        { codigo: 3, nome: "Cristiano Soares", telefone: "43999999999" },
-        { codigo: 4, nome: "Marcia Soria", telefone: "43999999999" },
-        { codigo: 5, nome: "Joao Teodoro", telefone: "43999999999" },
-        { codigo: 6, nome: "Nelson Cardoso", telefone: "43999999999" }
-    ]);
-
+export default function LocutorPage() {
     return (
         <div>
+            <HelmetProvider>
+                <Helmet>
+                    <title>{"EducadoraGS - Locutores"}</title>
+                </Helmet>
+            </HelmetProvider>
+
             <MenuApp />
-            <FormLocutor locutor={locutor} setLocutor={setLocutor} locutores={locutores} />
+            <FormLocutor />
             <Rodape />
         </div>
     )
