d7df3d66a1d57accb49027adf4e4fb1ff3588824
Lucas Rezende <lucasffrezende@gmail.com>
2024-05-23 14:20:11 -0300

    ✨ Adicionado title e components page de spots

--- src/pages/Spot.jsx
+++ src/pages/Spot.jsx
@@ -1,10 +1,20 @@
 import MenuApp from "../components/MenuApp";
+import { Helmet, HelmetProvider } from "react-helmet-async";
+import FormSpot from "../components/spots/FormSpot";
+import Rodape from "../components/Rodape";
 
 export default function PageSpot() {
     return (
         <div>
-            <MenuApp/>
-            <h1>Pagina de Spots</h1>
+            <HelmetProvider>
+                <Helmet>
+                    <title>{"EducadoraGS - Spots"}</title>
+                </Helmet>
+            </HelmetProvider>
+
+            <MenuApp />
+            <FormSpot />
+            <Rodape />
         </div>
     )
 }
