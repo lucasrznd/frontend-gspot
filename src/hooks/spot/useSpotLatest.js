e51d8bb7a7ac30c708a8c1ffcc4358322f263413
Lucas Rezende <lucasffrezende@gmail.com>
2024-06-11 11:00:00 -0300

    ♻️ Alterado URL para latest spot

--- src/hooks/spot/useSpotLatest.js
+++ src/hooks/spot/useSpotLatest.js
@@ -1,7 +1,7 @@
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 
-const API_URL = process.env.REACT_APP_API_BASE_URL + '/spot/latest-spots';
+const API_URL = process.env.REACT_APP_API_BASE_URL + '/spot/latest';
 
 const fetchData = async () => {
     const response = await axios.get(API_URL);
