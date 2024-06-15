ca94e8b46869f9f1af401382c211d5cc4be6b826
Lucas Rezende <lucasffrezende@gmail.com>
2024-06-15 10:33:40 -0300

    ✨ Adicionado hooks de topMonthly

---
+++ src/hooks/top_monthly/useAnnouncerTopMonthly.js
@@ -0,0 +1,19 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+
+const API_URL = process.env.REACT_APP_API_BASE_URL + '/monthly/announcer';
+
+const fetchData = async () => {
+    const response = await axios.get(API_URL);
+    return response.data;
+}
+
+export function useAnnouncerTopMonthly() {
+    const query = useQuery({
+        queryFn: fetchData,
+        queryKey: ['topMonthlyAnnouncer'],
+        refetchInterval: 60 * 5 * 1000
+    });
+
+    return query;
+}
---
+++ src/hooks/top_monthly/useCompanyTopMonthly.js
@@ -0,0 +1,19 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+
+const API_URL = process.env.REACT_APP_API_BASE_URL + '/monthly/company';
+
+const fetchData = async () => {
+    const response = await axios.get(API_URL);
+    return response.data;
+}
+
+export function useCompanyTopMonthly() {
+    const query = useQuery({
+        queryFn: fetchData,
+        queryKey: ['topMonthlyCompany'],
+        refetchInterval: 60 * 5 * 1000
+    });
+
+    return query;
+}
