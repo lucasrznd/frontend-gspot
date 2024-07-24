d1f74110223bb387784addee59fae86d5621da02
Lucas Rezende <lucasffrezende@gmail.com>
2024-07-24 09:00:14 -0300

    chore: change endpoint

--- src/hooks/top_monthly/useAnnouncerTopMonthly.js
+++ src/hooks/top_monthly/useAnnouncerTopMonthly.js
@@ -1,7 +1,7 @@
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 
-const API_URL = process.env.REACT_APP_API_BASE_URL + '/monthly/announcer';
+const API_URL = process.env.REACT_APP_API_BASE_URL + '/top/announcers';
 
 const fetchData = async () => {
     const response = await axios.get(API_URL);
--- src/hooks/top_monthly/useCompanyTopMonthly.js
+++ src/hooks/top_monthly/useCompanyTopMonthly.js
@@ -1,7 +1,7 @@
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 
-const API_URL = process.env.REACT_APP_API_BASE_URL + '/monthly/company';
+const API_URL = process.env.REACT_APP_API_BASE_URL + '/top/companies';
 
 const fetchData = async () => {
     const response = await axios.get(API_URL);
